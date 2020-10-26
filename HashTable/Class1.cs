a0d886343b7a3432ff03f4d57e85e812e0406cbc
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-26 11:01:53 +0530

    HashTable Demo

---
+++ HashTable/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    public class MapNode<K,V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K,V>>[] items;
+        public MapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPos(K key)
+        {
+            int pos = key.GetHashCode() % size;
+            return Math.Abs(pos);
+        }
+        public V Get(K key)
+        {
+            int pos = GetArrayPos(key);
+            LinkedList<KeyValue<K, V>> ll = GetLL(pos);
+            foreach(KeyValue<K,V> item in ll)
+            {
+                if (item.Key.Equals(key))
+                    return item.Value;
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int pos = GetArrayPos(key);
+            LinkedList<KeyValue<K, V>> ll = GetLL(pos);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            ll.AddLast(item);
+        }
+        public void Remove(K key,V value)
+        {
+            int pos = GetArrayPos(key);
+            LinkedList<KeyValue<K, V>> ll = GetLL(pos);
+            bool itemfound = false;
+            KeyValue<K, V> foundItem = default( KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in ll)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemfound = false;
+                    foundItem = item;
+                }
+            }
+            if (itemfound)
+                ll.Remove(foundItem);
+        }
+        protected LinkedList<KeyValue<K,V>> GetLL(int pos)
+        {
+            LinkedList<KeyValue<K, V>> ll = items[pos];
+            if(ll==null)
+            {
+                ll = new LinkedList<KeyValue<K, V>>();
+                items[pos] = ll;
+            }
+            return ll;
+
+        }
+    }
+
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+
+    }
+}
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -7,6 +7,17 @@ namespace HashTable
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to HashTable Demo!");
+            MapNode<string, string> hash = new MapNode<string, string>(5);
+            hash.Add("0", "A");
+            hash.Add("1", "y");
+            hash.Add("2", "ed");
+            hash.Add("3", "t");
+            hash.Add("4", "g");
+
+            string h4 = hash.Get("4");
+            Console.WriteLine("4th index value: " + h4);
+            //string h2 = hash.Get("2");
+
         }
     }
 }
